dd74104ed103c8b3e3572192e018815d5a3b743f
Jannik Walter <jannikwalter@Janniks-MacBook-Pro.local>
2020-05-19 18:08:18 +0200

    added deployment

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t jkw935/multi-client -t jkw935/multi-client:$SHA  -f ./client/Dockerfile ./client
-docker build -t jkw935/multi-server -t jkw935/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t jkw935/multi-worker -t jkw935/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t jkw5/multi-client -t jkw5/multi-client:$SHA  -f ./client/Dockerfile ./client
+docker build -t jkw5/multi-server -t jkw5/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t jkw5/multi-worker -t jkw5/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push jkw935/multi-client:latest
-docker push jkw935/multi-server:latest
-docker push jkw935/multi-worker:latest
+docker push jkw5/multi-client:latest
+docker push jkw5/multi-server:latest
+docker push jkw5/multi-worker:latest
 
-docker push jkw935/multi-client:$SHA
-docker push jkw935/multi-server:$SHA
-docker push jkw935/multi-worker:$SHA
+docker push jkw5/multi-client:$SHA
+docker push jkw5/multi-server:$SHA
+docker push jkw5/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=jkw935/multi-server:$SHA
-kubectl set image deployments/client-deployment server=jkw935/multi-client:$SHA
-kubectl set image deployments/worker-deployment server=jkw935/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=jkw5/multi-server:$SHA
+kubectl set image deployments/client-deployment server=jkw5/multi-client:$SHA
+kubectl set image deployments/worker-deployment server=jkw5/multi-worker:$SHA
