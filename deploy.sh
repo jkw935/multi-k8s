0d5aa396348fbe1275551eb618febf242624ba8d
Jannik Walter <jannikwalter@Janniks-MacBook-Pro.local>
2020-05-19 18:16:16 +0200

    added deployment

--- deploy.sh
+++ deploy.sh
@@ -12,5 +12,5 @@ docker push jkw5/multi-worker:$SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=jkw5/multi-server:$SHA
-kubectl set image deployments/client-deployment server=jkw5/multi-client:$SHA
-kubectl set image deployments/worker-deployment server=jkw5/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=jkw5/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=jkw5/multi-worker:$SHA
